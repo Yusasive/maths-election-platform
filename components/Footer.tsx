f4b907bf9e4b5d965da6487773ce821b5664edf3
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-06-22 06:46:30 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,18 @@
 export const Footer = () => {
-    return (
-      <footer className="bg-gray-100 pt-10 pb-3 flex flex-col items-center space-y-1 ">
-        <p className="font-semibold italic text-lg text-gray-700 text-center">
-          Crafted & Developed by
-          <a
-            href="https://www.linkedin.com/in/yuusuf-abdullahi-temidayo-yusasive"
-            className="text-blue-500 hover:text-blue-700 transition">
-            Yusasive
-          </a>
-        </p>
-        <p className="text-sm text-gray-500">
-          © {new Date().getFullYear()} All rights reserved.
-        </p>
-      </footer>
-    );
-  };
-  
+  return (
+    <footer className="bg-gray-100 pt-10 pb-3 flex flex-col items-center space-y-1 ">
+      <p className="font-semibold italic text-lg text-gray-700 text-center">
+        Crafted & Developed by {""}
+        <a
+          href="https://www.linkedin.com/in/yuusuf-abdullahi-temidayo-yusasive"
+          className="text-blue-500 hover:text-blue-700 transition"
+        >
+          Yusasive
+        </a>
+      </p>
+      <p className="text-sm text-gray-500">
+        © {new Date().getFullYear()} All rights reserved.
+      </p>
+    </footer>
+  );
+};
